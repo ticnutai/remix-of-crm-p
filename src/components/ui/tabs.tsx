152b62c28a996b4dfc5e990af13c54707edd5dd1
jj121 <office.tenarch@gmail.com>
2026-07-01 23:09:25 +0300

    Hide inactive tab panels

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -230,7 +230,7 @@ const TabsContent = React.forwardRef<
       ref={ref}
       value={value}
       className={cn(
-        "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+        "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[state=inactive]:!hidden",
         className,
       )}
       {...props}
